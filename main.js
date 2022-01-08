b9657b22907f1d21cbb5823507b604cf20f0c26c
K3MD1 <89929091+K3MD1@users.noreply.github.com>
2022-01-08 11:57:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ttiYjylLP/model.json',modelLoaded);
 
 function modelLoaded()
 {
